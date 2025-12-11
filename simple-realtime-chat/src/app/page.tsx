e5121c481ed453acb2f2ff2bf2e16a8875434d23
Andres <marianoandres191200@hotmail.com>
2025-12-10 22:39:45 -0300

    setting and getting username from localstorage

--- simple-realtime-chat/src/app/page.tsx
+++ simple-realtime-chat/src/app/page.tsx
@@ -1,9 +1,28 @@
 "use client";
+import { useEffect, useState } from "react";
 import useLocalStorage from "./shared/hooks/useLocalStorage";
 import styles from "./styles.module.scss";
 
+const USERNAME_KEY = "username" as const;
+
 export default function Home() {
   const { setItem, getItem } = useLocalStorage();
+  const [userName, setUserName] = useState<string>("");
+
+  useEffect(() => {
+    function setInitialUserName() {
+      const userName = getItem(USERNAME_KEY);
+      if (!userName) {
+        // ASSIGN RANDOM NAME WITH UUID
+        const username = "anonymous-walrus-2-e3c20s";
+        setItem(USERNAME_KEY, username);
+        setUserName(username);
+      } else {
+        setUserName(userName as string);
+      }
+    }
+    setInitialUserName();
+  }, []);
 
   return (
     <div className={styles.homePage}>
@@ -11,7 +30,7 @@ export default function Home() {
         <div className={styles.createRoomContainer}>
           <div>
             <h5 className={styles.createRoomText}>Welcome </h5>
-            <h5 className={styles.username}>anonymous-walrus-e3c20s</h5>
+            <h5 className={styles.username}>{userName}</h5>
           </div>
           <div className={styles.createRoomWarningMessageBox}>
             <span>
