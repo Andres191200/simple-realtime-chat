71edd45c734e5d095c68d3fa68ad1b437ecb3909
Andres <marianoandres191200@hotmail.com>
2026-02-08 18:50:23 -0300

    adding elysia auth validation middleware to messages POST endpoint

--- simple-realtime-chat/src/app/api/[[...slugs]]/route.ts
+++ simple-realtime-chat/src/app/api/[[...slugs]]/route.ts
@@ -1,6 +1,8 @@
 import { redis } from "@/lib/redis";
 import { Elysia } from "elysia";
 import { nanoid } from "nanoid";
+import { authMiddleware } from "./auth";
+import z from "zod";
 
 const ROOM_TTL_SECONDS = 600;
 
@@ -19,6 +21,15 @@ const createRoom = new Elysia({ prefix: "/rooms" }).post(
     return { roomId };
   }
 );
+
+const messages = new Elysia({ prefix: "/messages"}).use(authMiddleware).post("/", ({body, auth }) => {
+  const {sender, text} = body;
+
+
+}, {body: z.object({
+  sender: z.string().min(1).max(100),
+  text: z.string().min(1).max(200),
+})});
 
 const app = new Elysia({ prefix: "/api" }).use(createRoom);
 
