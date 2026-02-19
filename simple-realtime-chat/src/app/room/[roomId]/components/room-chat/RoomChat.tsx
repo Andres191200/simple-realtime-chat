347895c8f748a86dab78b9d6060197b4e1f340bb
Andres <marianoandres191200@gmail.com>
2026-02-19 16:31:25 -0300

    roomprovider

--- simple-realtime-chat/src/app/room/[roomId]/components/room-chat/RoomChat.tsx
+++ simple-realtime-chat/src/app/room/[roomId]/components/room-chat/RoomChat.tsx
@@ -3,12 +3,20 @@ import { useMutation } from "@tanstack/react-query";
 import styles from "./styles.module.scss";
 import { sendMessage } from "@/app/room/actions/send-message";
 import { edenClient } from "../../../../../lib/eden-client";
+import { useRoom } from "../../context/RoomContext";
+
+const TSendMessage = {
+  username: String,
+  text: String,
+}
+
 export default function RoomChat() {
-  const { } = useMutation({mutationFn: async (_) => {
+  const { roomId } = useRoom();
+  const { mutate: sendMessage } = useMutation({mutationFn: async ({username, text}: {username: string, text: string}) => {
     await edenClient.messages.post({
       sender: username,
       text: message,
-    }, {query: roomId})
+    }, {query: { roomId }})
   })
   return (
     <div className={styles.roomChatComponent}>
--- simple-realtime-chat/src/app/room/[roomId]/page.tsx
+++ simple-realtime-chat/src/app/room/[roomId]/page.tsx
@@ -2,13 +2,16 @@
 import styles from "./styles.module.scss";
 import RoomHeader from "./components/room-header/RoomHeader";
 import RoomChat from "./components/room-chat/RoomChat";
+import { RoomProvider } from "./context/RoomContext";
 
 export default function Room() {
   return (
-    <div className={styles.roomPage}>
-      <RoomHeader />
-      <div className="separator"/>
-      <RoomChat />
-    </div>
+    <RoomProvider>
+      <div className={styles.roomPage}>
+        <RoomHeader />
+        <div className="separator"/>
+        <RoomChat />
+      </div>
+    </RoomProvider>
   );
 }
