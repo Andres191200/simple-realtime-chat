a55c0bd8a22f3c4c72d75b57de833ae5e666a1cd
Andres <marianoandres191200@gmail.com>
2026-02-19 16:20:46 -0300

    save

--- simple-realtime-chat/src/app/room/[roomId]/components/room-chat/RoomChat.tsx
+++ simple-realtime-chat/src/app/room/[roomId]/components/room-chat/RoomChat.tsx
@@ -1,14 +1,24 @@
 "use client";
+import { useMutation } from "@tanstack/react-query";
 import styles from "./styles.module.scss";
 import { sendMessage } from "@/app/room/actions/send-message";
+import { edenClient } from "../../../../../lib/eden-client";
 export default function RoomChat() {
+  const { } = useMutation({mutationFn: async (_) => {
+    await edenClient.messages.post({
+      sender: username,
+      text: message,
+    }, {query: roomId})
+  })
   return (
     <div className={styles.roomChatComponent}>
         <section className={styles.chatBox}></section>
         <section className={styles.chatBottom}>
           <form action={sendMessage}>
             <input className={styles.chatInput} type="text" name="message" placeholder="Type your message"/>
-            <button type="submit" className={styles.sendButton}>Send</button>
+            <button type="submit" className={styles.sendButton} onClick={() => {
+               
+            }}>Send</button>
           </form>
         </section>
     </div>
