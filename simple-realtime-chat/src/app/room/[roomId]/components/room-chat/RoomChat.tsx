82f181f983f06bbdfea32f14b96f8601030b44c3
Andres <marianoandres191200@gmail.com>
2026-02-19 17:59:05 -0300

    removed logs

--- simple-realtime-chat/src/app/room/[roomId]/components/room-chat/RoomChat.tsx
+++ simple-realtime-chat/src/app/room/[roomId]/components/room-chat/RoomChat.tsx
@@ -1,34 +1,61 @@
 "use client";
 import { useMutation } from "@tanstack/react-query";
 import styles from "./styles.module.scss";
-import { sendMessage } from "@/app/room/actions/send-message";
-import { edenClient } from "../../../../../lib/eden-client";
 import { useRoom } from "../../context/RoomContext";
+import { FormEvent } from "react";
+import useLocalStorage from "../../../../shared/hooks/useLocalStorage";
+import { edenClient } from "../../../../../lib/eden-client";
 
-const TSendMessage = {
-  username: String,
-  text: String,
-}
+type TSendMessage = {
+  username: string;
+  text: string;
+};
 
 export default function RoomChat() {
   const { roomId } = useRoom();
-  const { mutate: sendMessage } = useMutation({mutationFn: async ({username, text}: {username: string, text: string}) => {
-    await edenClient.messages.post({
-      sender: username,
-      text: message,
-    }, {query: { roomId }})
-  })
+  const { getItem } = useLocalStorage();
+
+  const { mutate: sendMessage, isPending } = useMutation({
+    mutationFn: async ({ username, text }: TSendMessage) => {
+      await edenClient.messages.post(
+        {
+          sender: username,
+          text: text,
+        },
+        { query: { roomId } },
+      );
+    },
+  });
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const text = formData.get("message") as string;
+    const username = getItem("username") as string;
+
+    if (text.trim()) {
+      sendMessage({ username, text });
+      e.currentTarget.reset();
+    }
+  }
+
   return (
     <div className={styles.roomChatComponent}>
-        <section className={styles.chatBox}></section>
-        <section className={styles.chatBottom}>
-          <form action={sendMessage}>
-            <input className={styles.chatInput} type="text" name="message" placeholder="Type your message"/>
-            <button type="submit" className={styles.sendButton} onClick={() => {
-               
-            }}>Send</button>
-          </form>
-        </section>
+      <section className={styles.chatBox}></section>
+      <section className={styles.chatBottom}>
+        <form onSubmit={handleSubmit}>
+          <input
+            className={styles.chatInput}
+            type="text"
+            name="message"
+            placeholder="Type your message"
+            disabled={isPending}
+          />
+          <button type="submit" className={styles.sendButton}>
+            Send
+          </button>
+        </form>
+      </section>
     </div>
-  )
+  );
 }
